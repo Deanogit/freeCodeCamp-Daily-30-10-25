a8b3b2e8a90be402b16b5e6cfacc63395f3e3f89
Dean Howe <hideanhowe@gmail.com>
2026-01-02 06:41:56 +0000

    testing

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
 // A prime number is a positive integer greater than 1 that is divisible only by 1 and itself. The first five prime numbers are 2, 3, 5, 7, and 11.
 
 // Given a positive integer n, return the nth prime number. For example, given 5 return the 5th prime number: 11.
+
+function nthPrime(n) {
+  console.log(n);
+  const arr = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i % 1 === 0 && i % i === 0) {
+      arr.push(i);
+    }
+  }
+  // divisible by one and itself
+  if (n % 1 === 0 && n % n === 0) {
+    console.log('Prime');
+  }
+  console.log(arr);
+  return n;
+}
